0e091f153a15231305297788148f822e1f65629d
Wil Cook <wil.cook.is@gmail.com>
2018-11-20 15:01:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,11 +4,17 @@ def speak_to_grandma(string)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-  if string == string.up
+  if string != string.upcase
+    return "HUH?! SPEAK UP, SONNY!"
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
-
 # NO, NOT SINCE 1938!
+  elsif string == string.upcase
+    return "NO, NOT SINCE 1938!"
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+  else string == "I LOVE YOU, GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
+end
