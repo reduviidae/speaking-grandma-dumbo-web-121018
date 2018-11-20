5556d8ddbb110d8c3db4de2c9fb522f0557627c5
Wil Cook <wil.cook.is@gmail.com>
2018-11-20 14:58:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(string)
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+  if string == string.up
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
